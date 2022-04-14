09550ed5c9e60a3ac1f56c997a50dc90cef6ba48
Arvind <Arvind@DESKTOP-B0K1F0M.Dlink>
2022-04-14 04:29:23 -0700

    webapp folder structure complete

--- src/main/java/com/advatix/AgroProject/HomeController.java
+++ src/main/java/com/advatix/AgroProject/HomeController.java
@@ -1,15 +1,16 @@
 package com.advatix.AgroProject;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/index")
 	public String home() {
 		
-		return "index.jsp";
+		return "index";
 	}
 
 }
